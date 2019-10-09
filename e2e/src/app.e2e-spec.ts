ea729fee7c515399f852af336c8dcc15d4bf4295
Chris Holmes <chrismholmes1994@gmail.com>
2019-10-09 19:28:35 +0100

    Add some basic e2e tests and update some styling

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,70 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser} from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('Angular 8 Hacker News App', () => {
+  const appPage = new AppPage();
 
-  beforeEach(() => {
-    page = new AppPage();
+  const appRoutes = {
+    top: 'Top',
+    latest: 'Latest',
+    ask: 'Ask',
+    show: 'Show',
+    jobs: 'Jobs'
+  };
+
+  beforeAll(() => {
+    appPage.navigateTo();
+    appPage.assertTitleText();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to hacker-news!');
+  it('should check that router is working correctly', () => {
+    appPage.assertActiveRouteText(appRoutes.top);
+    appPage.clickAppRoute(appRoutes.latest);
+    appPage.assertActiveRouteText(appRoutes.latest);
+    appPage.clickAppRoute(appRoutes.ask);
+    appPage.assertActiveRouteText(appRoutes.ask);
+    appPage.clickAppRoute(appRoutes.show);
+    appPage.assertActiveRouteText(appRoutes.show);
+    appPage.clickAppRoute(appRoutes.jobs);
+    appPage.assertActiveRouteText(appRoutes.jobs);
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  describe('should check that data is loaded correctly', () => {
+
+    it('should check the top stories section', () => {
+      appPage.clickAppRoute(appRoutes.top);
+      appPage.isDataLoaded();
+    });
+
+    it('should check the latest stories section', () => {
+      appPage.clickAppRoute(appRoutes.latest);
+      appPage.isDataLoaded();
+    });
+
+    it('should check the ask stories section', () => {
+      appPage.clickAppRoute(appRoutes.ask);
+      appPage.isDataLoaded();
+    });
+
+    it('should check the show stories section', () => {
+      appPage.clickAppRoute(appRoutes.show);
+      appPage.isDataLoaded();
+    });
+
+    it('should check the job stories section', () => {
+      appPage.clickAppRoute(appRoutes.jobs);
+      appPage.isDataLoaded();
+    });
+  });
+
+  it('should check last active route is saved by cookie service', () => {
+    browser.waitForAngularEnabled(false);
+    appPage.clickFirstExternalStoryUrlFound();
+    browser.navigate().back();
+
+    browser.sleep(5000); // Sleep for 5 seconds to handle navigating back
+    appPage.assertActiveRouteText(appRoutes.jobs);
+    appPage.isDataLoaded();
+
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,51 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  assertTitleText() {
+    element(by.xpath('//span[contains(@class, "title")]')).getText().then(text => {
+      expect(text).toBe('Hacker News');
+    });
+  }
+
+  clickAppRoute(routeText: string) {
+    element(by.xpath('//a[contains(text(), "' + routeText + '")]')).click();
+  }
+
+  clickFirstExternalStoryUrlFound() {
+    element.all(by.xpath('//app-story-item//div[contains(@class, "url")]')).first().click();
+  }
+
+  assertActiveRouteText(activeRouteText: string) {
+    element(by.xpath('//a[contains(@class, "mat-tab-label-active")]')).getText().then(text => {
+      expect(text).toBe(activeRouteText);
+    });
+  }
+
+  isDataLoaded() {
+    element.all(by.xpath('//app-story-item')).count().then(count => {
+      expect(count).toBeGreaterThan(0);
+
+      const loadMoreButton = element(by.partialButtonText('Load more stories'));
+
+      if (count === 30) {
+        loadMoreButton.isPresent().then(result => {
+          expect(result).toBe(true);
+          this.clickLoadMoreButton(count);
+        });
+      } else {
+        loadMoreButton.isPresent().then(result => {
+          expect(result).toBe(false);
+        });
+      }
+
+    });
+  };
+
+  clickLoadMoreButton(count: number) {
+    element(by.partialButtonText('Load more stories')).click().then(() => {
+      element.all(by.xpath('//app-story-item')).count().then(newCount => {
+        expect(newCount).toBeGreaterThan(count);
+      });
+    });
   }
 }
